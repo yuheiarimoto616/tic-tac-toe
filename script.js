58964c1bf071bd39459529dc6b1ce60f2d0610db
yuheiarimoto616 <yuhei61627@icloud.com>
2023-07-16 23:54:31 +0900

    function to render the contents of the board done

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-const Gameboard = (() => {
-    const board = [];
-    const player1 = player("Yuhei", 'O');
-    const player2 = player("Arimoto", 'X');
-})();
-
 const player = (playerName, sign) => {
     const getName = () => playerName;
     const getSign = () => sign;
 
     return {getName, getSign};
 };
+
+const Gameboard = (() => {
+    const dimension = 3;
+    const board = [["◯", "✕", "✕"], 
+                  ["◯", "✕", "✕"], 
+                  ["◯", "✕", "✕"]];
+    const player1 = player("Yuhei", 'O');
+    const player2 = player("Arimoto", 'X');
+
+    const displayBoard =  () => {
+        console.log("started");
+        for (let i = 0; i < dimension; i++) {
+            for (let j = 0; j < dimension; j++) {
+                let sign = board[i][j];
+
+                let cell = document.getElementById(`${i}${j}`);
+                cell.textContent = sign;
+            }
+        }
+    };
+
+    return {displayBoard};
+})();
+
+Gameboard.displayBoard();
