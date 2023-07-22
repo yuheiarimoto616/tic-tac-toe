431ec14716447dcb3af7426fb1e8eff361e26c79
yuheiarimoto616 <yuhei61627@icloud.com>
2023-07-22 23:55:03 +0900

    GameController object implemented

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const player = (playerName, sign) => {
-    const getName = () => playerName;
-    const getSign = () => sign;
+const CIRCLE = "◯";
+const CROSS = "✕";
 
-    return {getName, getSign};
-};
-
-const Gameboard = (() => {
+const Gameboard = () => {
     const dimension = 3;
-    const board = [["◯", "✕", "✕"], 
-                  ["◯", "✕", "✕"], 
-                  ["◯", "✕", "✕"]];
-    const player1 = player("Yuhei", 'O');
-    const player2 = player("Arimoto", 'X');
+    const board = [];
+
+    for (let i = 0; i < dimension; i++) {
+        board[i] = [];
+        for (let j = 0; j < dimension; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+    const fillCell = (row, column, player) => {
+        board[row][column].addPlayer(player);
+    };
 
     const displayBoard =  () => {
-        console.log("started");
         for (let i = 0; i < dimension; i++) {
             for (let j = 0; j < dimension; j++) {
                 let sign = board[i][j];
@@ -25,7 +29,59 @@ const Gameboard = (() => {
         }
     };
 
-    return {displayBoard};
+    return {displayBoard, getBoard, fillCell};
+};
+
+const Cell = () => {
+    let player = null;
+
+    const addPlayer = (p) => {
+        player = p;
+    } 
+
+    const getPlayer = () => player;
+
+    return {addPlayer, getPlayer};
+}
+
+const Player = (playerName, Token) => {
+    const getName = () => playerName;
+    const getToken = () => Token;
+
+    return {getName, getToken};
+};
+
+const GameController = (() => {
+    let playerOne;
+    let playerTwo;
+    let gameBoard = Gameboard();
+
+    const setPlayerOne = (player) => {
+        playerOne = player;
+    };
+
+    const setPlayerTwo = (player) => {
+        playerTwo = player;
+    };
+
+    let activePlayer = playerOne;
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+    const playRound = (row, column) => {
+        gameBoard.fillCell(row, column, activePlayer);
+        switchPlayerTurn();
+    }
+
+    return {setPlayerOne, setPlayerTwo, switchPlayerTurn, getActivePlayer, playRound};
+})();
+
+const DisplayController = (() => {
+    
 })();
 
 Gameboard.displayBoard();
