a8c4b2a12c00f04437ab2456ce8fbf88a4e4e71d
yuheiarimoto616 <yuhei61627@icloud.com>
2023-07-23 02:45:36 +0900

    win logic implemented

--- script.js
+++ script.js
@@ -18,18 +18,51 @@ const Gameboard = () => {
         board[row][column].addPlayer(player);
     };
 
-    const displayBoard =  () => {
+    const checkWin = () => {
         for (let i = 0; i < dimension; i++) {
-            for (let j = 0; j < dimension; j++) {
-                let sign = board[i][j];
+            let win = true;
+            for (let j = 1; j < dimension; j++) {
+                if ((board[i][j].getPlayer() == null) || (board[i][j].getPlayer() !== board[i][j-1].getPlayer())) {
+                    win = false;
+                    break;
+                }
+            }
 
-                let cell = document.getElementById(`${i}${j}`);
-                cell.textContent = sign;
+            if (win) {
+                return true;
             }
         }
+
+        for (let i = 0; i < dimension; i++) {
+            let win = true;
+            for (let j = 1; j < dimension; j++) {
+                if ((board[j][i].getPlayer() == null) || (board[j][i].getPlayer() !== board[j-1][i].getPlayer())) {
+                    win = false;
+                    break;
+                }
+            }
+
+            if (win) {
+                return true;
+            }
+        }
+
+        let winLR = true;
+        let winRL = true;
+        for (let i = 1; i < dimension; i++) {
+            if ((board[i][i].getPlayer() == null) || board[i][i].getPlayer() !== board[i-1][i-1].getPlayer()) {
+                winLR = false;
+            }
+
+            if ((board[i][dimension - 1 - i].getPlayer() == null) || board[i][dimension - 1 - i].getPlayer() !== board[i-1][dimension - 1 - (i - 1)].getPlayer()) {
+                winRL = false;
+            }
+        }
+
+        return winLR || winRL;
     };
 
-    return {displayBoard, getBoard, fillCell};
+    return {checkWin, getBoard, fillCell};
 };
 
 const Cell = () => {
@@ -41,7 +74,15 @@ const Cell = () => {
 
     const getPlayer = () => player;
 
-    return {addPlayer, getPlayer};
+    const getToken = () => {
+        if (player == null) {
+            return "";
+        } else {
+            return player.getToken();
+        }
+    }
+
+    return {addPlayer, getPlayer, getToken};
 }
 
 const Player = (playerName, Token) => {
@@ -52,8 +93,8 @@ const Player = (playerName, Token) => {
 };
 
 const GameController = (() => {
-    let playerOne;
-    let playerTwo;
+    let playerOne = Player("Yuhei", CIRCLE);
+    let playerTwo = Player("Arimoto", CROSS);
     let gameBoard = Gameboard();
 
     const setPlayerOne = (player) => {
@@ -74,14 +115,43 @@ const GameController = (() => {
 
     const playRound = (row, column) => {
         gameBoard.fillCell(row, column, activePlayer);
+        if (gameBoard.checkWin()) {
+            console.log(gameBoard.checkWin());
+        };
         switchPlayerTurn();
     }
 
-    return {setPlayerOne, setPlayerTwo, switchPlayerTurn, getActivePlayer, playRound};
+    const getBoard = () => gameBoard.getBoard();
+
+    return {setPlayerOne, setPlayerTwo, switchPlayerTurn, getActivePlayer, playRound, getBoard};
 })();
 
 const DisplayController = (() => {
-    
-})();
+    let cells = document.querySelectorAll('.cell');
 
-Gameboard.displayBoard();
+    const updateScreen =  () => {
+        const board = GameController.getBoard();
+
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board.length; j++) {
+                let sign = board[i][j].getToken();
+
+                let cell = document.getElementById(`${i}${j}`);
+                cell.textContent = sign;
+            }
+        }
+    };
+
+    const updateCell = (e) => {
+        let cellID = e.target.id;
+        GameController.playRound(parseInt(cellID[0]), parseInt(cellID[1]));
+        e.target.removeEventListener('click', updateCell);
+        updateScreen();
+    }
+
+    cells.forEach(cell => {
+        cell.addEventListener('click', updateCell);
+    });
+
+    updateScreen();
+})();
